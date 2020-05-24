d0831fadf99a8d6df3043d2581ea6442bc10284b
phenix3443 <phenix3443@gmail.com>
2020-05-24 20:41:02 +0800

    增加注释

--- 24-swap-nodes-in-pairs/solution.py
+++ 24-swap-nodes-in-pairs/solution.py
@@ -1,10 +1,16 @@
 # -*- coding:utf-8; -*-
 
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution:
     def swapPairs(self, head):
         preHead = ListNode(-1)
-        preHead.next = head
+        preHead.next = head  # 这里新增一个节点是因为头指针不会随着遍历一直到最后
 
         pre = preHead  # 假设pre是替换好的最后一个节点，那么只需要交换current(pre.next)和next(pre.next.next)即可
         while pre.next and pre.next.next:  # 只需要交换curr和follow即可
