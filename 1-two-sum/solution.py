e71dddd18d4f173514fc2886de8faeca2d620cef
phenix3443 <phenix3443@gmail.com>
2020-05-24 12:21:18 +0800

    使用空字典，避免原地比较

--- 1-two-sum/solution.py
+++ 1-two-sum/solution.py
@@ -1,11 +1,18 @@
 # -*- coding:utf-8; -*-
 class Solution:
     def twoSum(self, nums, target):
-        m = {nums[i]: i for i in range(len(nums))}
+        m = {}  # 使用空的集合，避免之前本身相加的判断
 
         for i in range(len(nums)):
             l = m.get(target - nums[i])
-            if l and l != i:
-                return [i, l]
+            if not (l is None):
+                return [l, i]  # 因为必定l<i
+
+            m[nums[i]] = i
 
         return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.twoSum([2, 7, 11, 15], 9))
