57754dc67ede954fa1a36d7886421d009ed9bb10
phenix3443 <phenix3443@gmail.com>
2020-05-25 00:22:12 +0800

    add new

--- 20-valid-parentheses/solution.py
+++ 20-valid-parentheses/solution.py
@@ -1,4 +1,28 @@
 # -*- coding:utf-8; -*-
 class Solution:
     def isValid(self, s):
-        pass
+        m = {")": "(", "]": "[", "}": "{"}
+        stack = []
+        for c in s:
+            if c not in m:  # 不是右括号
+                if c == " ":
+                    continue
+                else:
+                    stack.append(c)
+                    continue
+            else:
+                # 遇到右括号
+                if not stack:  # 栈空
+                    return False
+                else:
+                    if stack[len(stack) - 1] != m[c]:  # 顶端元素不匹配
+                        return False
+                    else:
+                        stack.pop()
+
+        return True if not stack else False
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isValid("()"))
