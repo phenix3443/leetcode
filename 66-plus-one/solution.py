32ce086480fd442e75323ae617f680423de8f14d
liushangliang <liushangliang@xunlei.com>
2020-05-25 13:35:56 +0800

    优化代码

--- 66-plus-one/solution.py
+++ 66-plus-one/solution.py
@@ -1,26 +1,18 @@
 # -*- coding:utf-8; -*-
 class Solution:
     def plusOne(self, digits):
-        nums = [0] + digits.copy()
-        flag = True  # 表示是否有进位，初始1可以看做是低位进位
-        for i in range(len(nums) - 1, -1, -1):
-            if not flag:  # 没有进位，中断
-                break
-            else:  # 有进位
-                if nums[i] < 9:
-                    nums[i] += 1  # 直接+1，不产生进位
-                    flag = False
-                else:
-                    nums[i] = 0  # 9置0，产生进位
-                    flag = True
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += 1
+            if digits[i] ^ 10:  # 异或判断是否不相等
+                return digits
+            else:
+                digits[i] &= 0
 
-        if nums[0] == 0:  # 判断最高位是否为0
-            return nums[1:]
-        else:
-            return nums
+        digits.insert(0, 1)  # 这个时候最高位肯定应该是1
+        return digits
 
 
 if __name__ == "__main__":
-    digits = [9, 9, 9, 9]
+    digits = [9]
     s = Solution()
     print(s.plusOne(digits))
