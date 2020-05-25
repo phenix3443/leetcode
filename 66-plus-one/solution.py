1802788af0f352d387de20e1e89427f5d2bec2dc
liushangliang <liushangliang@xunlei.com>
2020-05-25 14:20:15 +0800

    另外一种思路

--- 66-plus-one/solution.py
+++ 66-plus-one/solution.py
@@ -1,18 +1,23 @@
 # -*- coding:utf-8; -*-
 class Solution:
     def plusOne(self, digits):
+        lastNot9 = -1
         for i in range(len(digits) - 1, -1, -1):
-            digits[i] += 1
-            if digits[i] ^ 10:  # 异或判断是否不相等
-                return digits
+            if digits[i] ^ 9:
+                lastNot9 = i
+                break
             else:
                 digits[i] &= 0
 
-        digits.insert(0, 1)  # 这个时候最高位肯定应该是1
+        if lastNot9 == -1:
+            digits.insert(0, 1)  # 这个时候最高位肯定应该是1
+        else:
+            digits[lastNot9] += 1
+
         return digits
 
 
 if __name__ == "__main__":
-    digits = [9]
+    digits = [9, 9]
     s = Solution()
     print(s.plusOne(digits))
