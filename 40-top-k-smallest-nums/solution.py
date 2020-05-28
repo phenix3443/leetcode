c54697fabdd4d8e618195a08d5233817b75405a6
liushangliang <liushangliang@xunlei.com>
2020-05-28 17:47:27 +0800

    使用优先队列模拟

--- 40-top-k-smallest-nums/solution.py
+++ 40-top-k-smallest-nums/solution.py
@@ -1,6 +1,5 @@
 # - *-coding: utf - 8; - *-
-
-import heapq
+import queue
 
 
 class Solution:
@@ -11,7 +10,22 @@ class Solution:
         if len(arr) <= k:
             return arr
 
-        return heapq.nsmallest(k, arr)
+        q = queue.PriorityQueue(k)
+
+        for e in arr:
+            if not q.full():
+                q.put(0 - e)  # 以为优先队列默认是小根堆，但是需要将最大的数字挤出去
+            else:
+                e = -e
+                top = q.get()
+                if e > top:
+                    q.put(e)
+                else:
+                    q.put(top)
+
+        ret = [0 - q.get() for i in range(q.qsize())]
+        ret.reverse()
+        return ret
 
 
 if __name__ == "__main__":
