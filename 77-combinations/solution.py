bb4aae2473ce4eb56631f6c87282a5e71a50d6fb
phenix3443 <phenix3443@gmail.com>
2020-06-03 23:45:54 +0800

    递归算法

--- 77-combinations/solution.py
+++ 77-combinations/solution.py
@@ -3,11 +3,20 @@
 
 class Solution:
     def combine(self, n, k):
+        if k == 0:
+            return []
+
         result = []
 
-        def helper(n, k, l):
+        def helper(k, l):
             if len(l) == k:
                 result.append(l)
                 return
+
+            start = l[-1] + 1 if l else 1
+            for m in range(start, n + 1):
+                helper(k, l + [m])
+
+        helper(k, [])
 
         return result
