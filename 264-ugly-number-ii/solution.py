8e1a4f81e071de7abe858fdde9d514d97bafaeb2
liushangliang <liushangliang@xunlei.com>
2020-06-08 14:08:23 +0800

    修改描述

--- 264-ugly-number-ii/solution.py
+++ 264-ugly-number-ii/solution.py
@@ -5,7 +5,8 @@ import heapq
 
 
 class SolutionV1:
-    """ 使用最小堆排序，时间复杂度O(nlogn)，空间复杂度O(2n)
+    """ 回溯解法：
+    使用最小堆排序，时间复杂度O(nlogn)，空间复杂度O(2n)
     """
 
     def nthUglyNumber(self, k):
@@ -18,7 +19,7 @@ class SolutionV1:
         nth = 0
         for i in range(k):
             nth = heapq.heappop(hp)  # 取出第i个丑数
-            for j in [2, 3, 5]:
+            for j in [2, 3, 5]:  # 为什么要执行这一步呢？是因为我们知道，后续的丑数都是当前这个最小的丑数*[2,3,5]得出的。
                 newNum = nth * j
                 if newNum not in existNums:
                     existNums.add(newNum)
@@ -28,10 +29,10 @@ class SolutionV1:
 
 
 class Solution:
-    """
+    """递归解法：
     dp和递归的区别就是dp记录了第n个解前面的状态，进而我们先尝试使用一维数组dp[n]，记录第n个丑数。
     根据上面V1的求解过程，我们可以得知，第i次的最小值是取集合中的最小值。集合是dp[0]...dp[n-1]分别乘以2,3,5，然后减去dp[0]到dp[n-1]。
-    最终 dp[n]=min(dp[0:n-1]*[2,3,5]-dp[0:n-1])
+    最终 dp[n]=min{dp[0:n-1]*[2,3,5]-dp[0:n-1]}
     dp[0:n-1]*[2,3,5] 这个集合可以表示为下面的形式：
     乘以2的结果集：dp[0]*2 , dp[1]*2 ... dp[n-1]*2
     乘以3的结果集：dp[0]*3 , dp[1]*3 ... dp[n-1]*3
@@ -42,7 +43,7 @@ class Solution:
     找到对应的i2,i3,i5后，因为对应数值已经在计算最小值的时候用了，所以应该递增1，比如选中dp[i]=dp[i2]*2，那么下一步i2+=1,
     又因为i2,i3,i5之间可能有交集，比如dp[6]=dp[3]*2 ,dp[6]=dp[2]*3,所以i2,i3,i5都已经做检查。
 
-    所以dpz状态方程：dp[n]=min(dp[i2]*2，dp[i3]*3,dp[i5]*5)
+    所以dpz状态方程：dp[n]=min{dp[i2]*2，dp[i3]*3,dp[i5]*5}
     初始条件dp[0]=1, i2=i3=i5=0
     """
 
