abe2205462dcdc6417ba4263a408b9dd752b39d8
liushangliang <liushangliang@xunlei.com>
2020-06-09 18:03:33 +0800

    先添加解题模板

---
+++ 322-coin-change/solution.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8; -*-
+class Solution:
+    """
+    动态规划思路：
+
+    """
+
+    def coinChange(self, coins, amount):
+        pass
+
+
+if __name__ == "__main__":
+    coins = [1, 2, 5]
+    amount = 11
+    s = Solution()
+    s.coinChange(coins, amount)
