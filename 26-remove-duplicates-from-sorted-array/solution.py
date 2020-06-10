8926dce13353455aa192ccbc6e90826db45b892d
liushangliang <liushangliang@xunlei.com>
2020-06-10 12:07:11 +0800

    优化代码，添加注释

--- 26-remove-duplicates-from-sorted-array/solution.py
+++ 26-remove-duplicates-from-sorted-array/solution.py
@@ -3,11 +3,10 @@
 
 class Solution:
     def removeDuplicates(self, nums):
-        lastUniq, i = 0, 0
-        while i < len(nums) - 1:
-            i += 1
-            if nums[i] != nums[lastUniq]:
+        lastUniq = 0  # 前面唯一序列的最后一个元素
+        for i, v in enumerate(nums[1:]):
+            if v != nums[lastUniq]:
                 lastUniq += 1
-                nums[lastUniq] = nums[i]
+                nums[lastUniq] = v
 
         return lastUniq + 1
