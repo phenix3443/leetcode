47c2e772423ccf57e7995a818c16d5208bd1f5d0
liushangliang <liushangliang@xunlei.com>
2020-06-10 11:54:17 +0800

    优化代码

--- 283-move-zeroes/solution.py
+++ 283-move-zeroes/solution.py
@@ -1,9 +1,16 @@
 # -*- coding:utf-8; -*-
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        zeroStart = 0
-        for i in range(len(nums)):
-            if nums[i]:  # 这里不使用num[i]!=0居然能加快速度
-                if zeroStart != i:  # 避免原地互换
-                    nums[zeroStart], nums[i] = nums[i], nums[zeroStart]
-                zeroStart += 1
+    def moveZeroes(self, nums):
+        preZero = -1  # 指向遍历元素前面最后一个非0元素
+        for i, v in enumerate(nums):
+            if v != 0:
+                preZero += 1
+                if preZero != i:  # 避免原地互换
+                    nums[preZero], nums[i] = nums[i], nums[preZero]
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5]
+    s = Solution()
+    s.moveZeroes(nums)
+    print(nums)
--- 322-coin-change/solution.py
+++ 322-coin-change/solution.py
@@ -1,16 +1,28 @@
 # -*- coding:utf-8; -*-
+import sys
+
+
 class Solution:
     """
     动态规划思路：
-
+    1. 状态定义：dp[i]表示 数值i对应的最少钱币数量。
+    2. 转移方程：dp[i] = min{dp[i-coins[j]]}+1  0<=j<len(coins)
+    3. 边界条件： dp[0] =0
     """
 
     def coinChange(self, coins, amount):
-        pass
+        dp = [0] * (amount + 1)
+        for i in range(1, amount + 1):
+            pre = [dp[i - j] for j in coins if i - j >= 0]
+            dp[i] = min(pre) + 1 if pre else sys.maxsize
+
+        return dp[amount] if dp[amount] < sys.maxsize else -1
 
 
 if __name__ == "__main__":
     coins = [1, 2, 5]
     amount = 11
+    # coins = [2]
+    # amount = 3
     s = Solution()
-    s.coinChange(coins, amount)
+    print(s.coinChange(coins, amount))
