fe1202ee41051e30d66764bd7427e1740dfc976c
liushangliang <liushangliang@xunlei.com>
2020-06-15 17:10:54 +0800

    add new

--- 102-binary-tree-level-order-traversal/solution.py
+++ 102-binary-tree-level-order-traversal/solution.py
@@ -1,4 +1,16 @@
 # -*- coding:utf-8; -*-
 class Solution:
+    """ 队列处理
+    https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/pythonicde-ji-jian-dai-ma-xie-fa-wu-xu-di-gui-wu-x/
+    """
+
     def levelOrder(self, root):
-        pass
+        res = []
+        currLevel = [(root,)]  # 这里处理比较巧妙
+        while currLevel:  # 每次处理不是单个node，而是整个列表
+            res.append([n.val for lrPairs in currLevel for n in lrPairs if n])
+            currLevel = [
+                (n.left, n.right) for lrPairs in currLevel for n in lrPairs if n
+            ]
+
+        return res[:-1]  # 因为最后一层的左右儿子都是null
