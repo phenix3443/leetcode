aae6c56c76daedee8e255f01a4aeddc9f1724b7e
phenix3443 <phenix3443@gmail.com>
2023-11-07 17:28:12 +0800

    feat: update

--- 15-3sum/solution.py
+++ 15-3sum/solution.py
@@ -2,13 +2,6 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        m = {}
-        for i, v in enumerate(nums):
-            if target - v in m:
-                return [m[target - v], i]
-            m[v] = i
-
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ret = set()
         nums.sort()  # 排序后方便two-sum夹逼
